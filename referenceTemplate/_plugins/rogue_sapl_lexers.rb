9df4be3c3a8aa82ad9b193e971a915d783b799e7
Dominic Heutelbeck <dominic@heutelbeck.com>
2026-03-04 11:04:40 +0100

    Update rogue_sapl_lexers.rb

--- referenceTemplate/_plugins/rogue_sapl_lexers.rb
+++ referenceTemplate/_plugins/rogue_sapl_lexers.rb
@@ -17,8 +17,7 @@ module Rouge
         rule %r/\/\*/, Comment::Multiline, :multiline_comment
         rule %r/"/, Str::Double, :string
         rule %r/-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/, Num
-        rule %r/\b(?:deny-overrides|permit-overrides|first-applicable|only-one-applicable|deny-unless-permit|permit-unless-deny)\b/, Name::Constant
-        rule %r/\b(?:policy|set|permit|deny|where|var|import|as|schema|enforced|obligation|advice|transform|for|each)\b/, Keyword
+        rule %r/\b(?:policy|set|permit|deny|var|import|as|schema|enforced|obligation|advice|transform|for|each|or|errors|abstain|propagate|first|priority|strict|unanimous|unique)\b/, Keyword
         rule %r/\b(?:true|false|null|undefined)\b/, Keyword::Constant
         rule %r/\b(?:subject|action|resource|environment)\b/, Name::Builtin
         rule %r/\|?<(?=[a-zA-Z_$])/, Operator, :attribute_finder
@@ -34,8 +33,8 @@ module Rouge
         rule %r/\|-/, Operator
         rule %r/::/, Operator
         rule %r/=/, Operator
-        rule %r/[|^&<>+\-*\/%!]/, Operator
-        rule %r/@/, Name::Variable::Instance
+        rule %r/[|^&<>+\-*\/%!?]/, Operator
+        rule %r/[@#]/, Name::Variable::Instance
         rule %r/\b[a-zA-Z_$][a-zA-Z0-9_$]*(?:\.[a-zA-Z_$][a-zA-Z0-9_$]*)+(?=\()/, Name::Function
         rule %r/\b[a-zA-Z_$][a-zA-Z0-9_$]*(?=\()/, Name::Function
         rule %r/\^?[a-zA-Z_$][a-zA-Z0-9_$]*/, Name
@@ -88,21 +87,21 @@ module Rouge
         rule %r/\/\*/, Comment::Multiline, :multiline_comment
         rule %r/"/, Str::Double, :string
         rule %r/[+-]?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?/, Num
-        rule %r/\b(?:deny-overrides|permit-overrides|only-one-applicable|deny-unless-permit|permit-unless-deny)\b/, Name::Constant
-        rule %r/\b(?:permit|deny|indeterminate|notApplicable)\b/, Name::Constant
-        rule %r/\b(?:pip|static-pip|function-library|static-function-library)\b/, Keyword::Type
-        rule %r/\b(?:requirement|scenario|given|when|expect|then)\b/, Keyword::Declaration
-        rule %r/\b(?:attempts|emits|maps|called|wait|matching|equals|containing|with|is|of|to|on|in|where|any)\b/, Keyword
-        rule %r/\b(?:text|number|boolean|array|object|null|blank|empty|regex|length|stream|order|error)\b/, Keyword::Type
-        rule %r/\b(?:policy|set|policies|pdp|function|attribute|decision|obligation|advice|virtual-time|environment)\b/, Keyword
+        rule %r/\b(?:permit|deny|indeterminate|not-applicable)\b/, Name::Constant
+        rule %r/\b(?:requirement|scenario|given|when|expect|then|verify)\b/, Keyword::Declaration
+        rule %r/\b(?:subject|action|resource|environment)\b/, Name::Builtin
+        rule %r/\b(?:attempts|emits|maps|called|matching|equals|containing|with|is|of|to|on|in|where|any|next|once|times|and|starting|ending)\b/, Keyword
+        rule %r/\b(?:text|number|boolean|array|object|blank|empty|regex|length|stream|order|error|null-or-empty|null-or-blank|equal|compressed|whitespace|case-insensitive)\b/, Keyword::Type
+        rule %r/\b(?:function|attribute|decision|obligation|advice|obligations|variables|secrets|configuration|pdp-configuration|document|documents|key|value|or|errors|abstain|propagate|first|priority|strict|unanimous|unique)\b/, Keyword
         rule %r/\b(?:true|false|null|undefined)\b/, Keyword::Constant
-        rule %r/[a-zA-Z_$][a-zA-Z0-9_$-]*/, Name
+        rule %r/[a-zA-Z_$][a-zA-Z0-9_$]*/, Name
         rule %r/[{}()\[\]:;,.<>-]/, Punctuation
       end
 
       state :string do
         rule %r/"/, Str::Double, :pop!
         rule %r/\\["\\\/bfnrt]/, Str::Escape
+        rule %r/\\u[0-9a-fA-F]{4}/, Str::Escape
         rule %r/\\./, Str::Double
         rule %r/[^"\\]+/, Str::Double
       end
